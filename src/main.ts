b0a03c91215342c3a75166b92750f06b0e06d1b5
Edgardo Palacios <edgard07hp@gmail.com>
2023-02-13 22:54:38 -0600

    🔧 Global validations

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix("api"); // ==> Config Global prefix
+  app.setGlobalPrefix('api'); // ==> Config Global prefix
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, // ==> Transform params to DTO
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   await app.listen(process.env.PORT);
   console.info(`App running on port ${process.env.PORT}`);
