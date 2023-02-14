ae993adea393cb4ec79dc2c346c72752a50a3168
Edgardo Palacios <edgard07hp@gmail.com>
2023-02-13 22:22:32 -0600

    🚧 Index - Static content

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ParkingModule } from './parking/parking.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ParkingModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    ParkingModule,
+  ],
 })
 export class AppModule {}
