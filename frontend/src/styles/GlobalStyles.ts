f89739a4263f466674f60bf76da368722b85ce14
Edgardo Palacios <edgard07hp@gmail.com>
2023-02-16 21:48:50 -0600

    💄 Add styles

--- frontend/src/styles/GlobalStyles.ts
+++ frontend/src/styles/GlobalStyles.ts
@@ -14,6 +14,12 @@ export const GlobalStyles = createGlobalStyle`
         margin-top: 30px;
     }
 
+    .iconTable {
+      width: 20px;
+      height: 20px;
+      cursor: pointer;
+    }
+
     button {
         border-radius: 34px;
         padding: 7px 25px;
@@ -36,4 +42,51 @@ export const GlobalStyles = createGlobalStyle`
         color: var(--color-primary) !important;
         font-weight: var(--semiBold-weight);
     }
+
+    /* Custom modals */
+  .ant-modal-content {
+    border-radius: 15px !important;
+    box-shadow: 0 0 20px 0 var(--color-black) !important;
+  }
+
+  .ant-modal-close {
+    border-radius: 15px !important;
+    background: var(--color-primary) !important;
+    background-color: var(--color-primary) !important;
+    color: var(--color-blue) !important;
+    border: none !important;
+    padding: 0 !important;
+  }
+
+  .ant-modal-close:hover {
+    color: var(--color-primary) !important;
+  }
+
+  .ant-modal-close {
+    position: absolute !important;
+    right: -30px !important;
+    top: -15px !important;
+    box-shadow: 0 0 20px 0 var(--color-black) !important;
+  }
+
+  .ant-modal-close:hover {
+    background: var(--color-secundary-gray) !important;
+  }
+
+  .ant-modal-close-x {
+    width: 22px !important;
+    height: 16px !important;
+  }
+
+  .ant-modal-close-x > .anticon {
+    vertical-align: 0.7em !important;
+  }
+
+  @media (max-width: 700px) {
+    .ant-modal-close {
+      position: absolute !important;
+      right: 10px !important;
+      top: 10px !important;
+    }
+  }
 `;
